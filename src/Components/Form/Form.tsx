4df8a6cdf7a9f06c6288063bab18854e33b85729
Your Name <you@example.com>
2021-12-16 20:18:36 +0100

    added route to pages

--- src/Components/Form/Form.tsx
+++ src/Components/Form/Form.tsx
@@ -1,59 +1,53 @@
 import { Typography } from '@material-ui/core'
 import React from 'react'
-import {useFormStyles} from './FormStyles'
-import {TextBox} from '../TextField/TextBox';
-import {Check} from '../CheckBox/Check'
-import {Button} from '../Button/Button'
-import { SelectField } from '../Select/SelectField';
-import {Slider} from  '../Carousel/Slider'
+import { useFormStyles } from './FormStyles'
+import { TextBox } from '../TextField/TextBox'
+import { Check } from '../CheckBox/Check'
+import { Button } from '../Button/Button'
+import { SelectField } from '../Select/SelectField'
+import { Slider } from '../Carousel/Slider'
 
-
-
-interface formProps{
-    scale?: string;
+interface formProps {
+  scale?: string
 }
-export const Form: React.FC<formProps> =() => {
-    const classes = useFormStyles()
-    return (
-      <div className={classes.accountPage}>
-        <div className={classes.formWrapper}>
-    <div className={classes.formCard}>
-    <div className={classes.formContent}>
-    <div className={classes.formLeft}>
-      <div className={classes.formInstruction} >
-        <Typography align='center' className={classes.createAcc}>Create an account</Typography> 
-        <Typography align='center' className={classes.continue}>Create an account to continue</Typography>
+export const Form: React.FC<formProps> = () => {
+  const classes = useFormStyles()
+  return (
+    <div className={classes.accountPage}>
+      <div className={classes.formWrapper}>
+        <div className={classes.formCard}>
+          <div className={classes.formContent}>
+            <div className={classes.formLeft}>
+              <div className={classes.formInstruction}>
+                <Typography align="center" className={classes.createAcc}>
+                  Create an account
+                </Typography>
+                <Typography align="center" className={classes.continue}>
+                  Create an account to continue
+                </Typography>
+              </div>
+              <TextBox
+                type=""
+                subject="Email"
+                placeholder="Topebanlosi@gmail.com"
+              />
+              <TextBox subject="Password" type="password" placeholder="" />
+              <TextBox
+                subject="Confirm password"
+                type="password"
+                placeholder=""
+              />
+
+              <SelectField label="" subject="Select currency" />
+              <Check color="primary" />
+              <Button color="primary" action="Get Started" />
+            </div>
+          </div>
         </div>
-         <TextBox 
-         type='' 
-         subject='Email'
-           placeholder='Topebanlosi@gmail.com' />
-         <TextBox subject='Password'
-         type='password' 
-           placeholder=''/>
-         <TextBox subject='Confirm password'
-          type='password'
-           placeholder='' />
-        
-        
-        <SelectField label=''
-        subject='Select currency'
-        />
-        <Check 
-        color='primary' />
-        <Button color='primary'
-        action='Get Started'
-        />
-        </div>
-        </div>
-       
-        </div>
-        </div>
-        <div className={classes.formRight}>
+      </div>
+      <div className={classes.formRight}>
         <Slider />
-        </div>
-        </div>
-    )
+      </div>
+    </div>
+  )
 }
-
-
