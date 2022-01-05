e696b2d9d6e7fd4e5faaf4931e352278e1848584
Your Name <you@example.com>
2022-01-05 18:04:12 +0100

    change home button color

--- src/Components/Button/ButtonStyles.ts
+++ src/Components/Button/ButtonStyles.ts
@@ -1,7 +1,7 @@
 import {makeStyles}  from "@material-ui/core/styles";
 export const useButtonStyles = makeStyles((theme) => ({
   btn:{
-      backgroundColor:theme.design.colors.blue,
+      backgroundColor:theme.design.colors.blue ,
       width: '75%',
       color:theme.design.colors.white,
       textTransform: "none",
